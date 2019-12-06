d9802594d3bc71c1a27ceda54a8a2cad202012c8
giraffetree <giraffetree1@gmail.com>
2019-12-06 11:11:47 +0800

    update: add slf4j + log4j2 + @slf4j

--- src/main/java/me/giraffetree/java/boomjava/utils/log/Log4j2Test.java
+++ src/main/java/me/giraffetree/java/boomjava/utils/log/Log4j2Test.java
@@ -13,22 +13,16 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 @Slf4j
 public class Log4j2Test {
+
+    private final static Marker BOY = MarkerFactory.getMarker("boy");
+    private final static Marker GIRL = MarkerFactory.getMarker("girl");
     private final static Marker OTHER = MarkerFactory.getMarker("other");
 
     public static void main(String[] args) {
-        long l1 = System.currentTimeMillis();
-        int loopCount = 100_000;
-        int count = loopCount;
-        int size = 10;
-        while (count > 0) {
-            writeMultipleUsersLog(size);
-            count--;
-        }
 
-        long l2 = System.currentTimeMillis();
-        System.out.println(String.format("size:%d loopCount:%d cost: %dms", size, loopCount, l2 - l1));
+        testRouting(100, 200);
+
     }
-
 
     public static void addRandomLog() {
         int num = ThreadLocalRandom.current().nextInt(0, 1000);
@@ -40,13 +34,27 @@ public class Log4j2Test {
         }
     }
 
+    public static void testRouting(int loopCount, int userSize) {
+        long l1 = System.currentTimeMillis();
+        int count = loopCount;
+        while (count > 0) {
+            writeMultipleUsersLog(userSize);
+            count--;
+        }
+
+        long l2 = System.currentTimeMillis();
+        System.out.println(String.format("size:%d loopCount:%d cost: %dms", userSize, loopCount, l2 - l1));
+    }
+
     public static void writeMultipleUsersLog(int size) {
         for (int i = 0; i < size; i++) {
             String userName = "user" + i;
             ThreadContext.put("logFileName", userName);
-            int num = ThreadLocalRandom.current().nextInt(0, 1000);
-            if (num < 950) {
-                log.info("current: {}", num);
+            int num = ThreadLocalRandom.current().nextInt(0, 1200);
+            if (num < 500) {
+                log.info(BOY, "current: {}", num);
+            } else if (num < 1000) {
+                log.info(GIRL, "current: {}", num);
             } else {
                 log.info(OTHER, "current: {}", num);
             }
