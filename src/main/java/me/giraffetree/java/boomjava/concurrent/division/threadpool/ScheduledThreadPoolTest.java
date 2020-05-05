a1b9b961cf8e87f502d1c1d3e7b949b8dea5a3d2
giraffetree <15355498770@163.com>
2020-05-05 23:03:26 +0800

    update: 新增 threadPool 监控/无界队列/延时任务等测试

---
+++ src/main/java/me/giraffetree/java/boomjava/concurrent/division/threadpool/ScheduledThreadPoolTest.java
@@ -0,0 +1,20 @@
+package me.giraffetree.java.boomjava.concurrent.division.threadpool;
+
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+
+/**
+ * ScheduledThreadPool 基于 DelayQueue, DelayQueue 基于 PriorityQueue
+ *
+ * @author GiraffeTree
+ * @date 2020-05-05
+ */
+public class ScheduledThreadPoolTest {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
