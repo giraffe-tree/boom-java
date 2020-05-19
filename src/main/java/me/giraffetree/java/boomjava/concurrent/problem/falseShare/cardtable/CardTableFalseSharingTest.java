c7f324370715a5f69b27ddea50f85a81e40924a3
giraffetree <15355498770@163.com>
2020-05-20 00:25:44 +0800

    update: 整理了下 card table 中产生 false sharing 问题的原因, 测试成功!

--- src/main/java/me/giraffetree/java/boomjava/concurrent/problem/falseShare/cardtable/CardTableFalseSharingTest.java
+++ src/main/java/me/giraffetree/java/boomjava/concurrent/problem/falseShare/cardtable/CardTableFalseSharingTest.java
@@ -9,27 +9,36 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 测试失败!!
  * 用于测试在 hotspot 中 卡表 False Sharing
- * -XX:+UseCondCardMark 使用 jdk11 运行
- * https://github.com/unofficial-openjdk/openjdk/blob/f2757c6c03e0000047d2b32f6198f9bc9a46e9d0/test/hotspot/jtreg/gc/CondCardMark/Basic.java
+ * -XX:+UseCondCardMark 使用 jdk8 运行
  * 由于64字节的卡表 表示了 32KB 的内存
  * 使得两个对象之间尽可能地公用同一个卡表
  * 测试结果:
- * 和预期的结果不符
- * 使用 jdk11 运行
- * 参数 `-XX:-TieredCompilation`
- * volatile average:66ms [92, 69, 63, 63, 63, 65, 63, 64, 63, 64]
- * 使用 `-XX:+UseCondCardMark -XX:-TieredCompilation`
- * volatile average:67ms [87, 67, 65, 66, 64, 64, 66, 65, 64, 64]
- * 两者几乎没有差距?
- * 再用 jdk7 试了下
- * > D:\env\jdk7\jdk\bin\javac -encoding utf8 CardTableFalseSharingTest.java
- * > D:\env\jdk7\jdk\bin\java -XX:+UseCondCardMark CardTableFalseSharingTest
- * volatile average:84ms [101, 84, 81, 85, 80, 82, 81, 87, 70, 90]
- * > D:\env\jdk7\jdk\bin\java  CardTableFalseSharingTest
- * volatile average:89ms [81, 70, 88, 97, 83, 82, 84, 92, 106, 113]
- * 差距还是不大 =.= 这就有点不知道怎么回事了
+ * jdk1.8.0_161 上运行
+ * 不使用
+ * volatile average:158ms [257, 111, 116, 210, 164, 131, 187, 120, 146, 147]
+ * volatile average:167ms [228, 188, 148, 133, 169, 180, 159, 130, 183, 154]
+ * 使用  -XX:+UseCondCardMark
+ * volatile average:26ms [116, 17, 12, 13, 12, 13, 27, 31, 12, 14]
+ * volatile average:40ms [117, 23, 33, 33, 34, 32, 34, 33, 32, 33]
+ * 使用 -XX:-TieredCompilation
+ * volatile average:158ms [242, 146, 144, 137, 146, 178, 138, 159, 146, 145]
+ * 使用 -XX:-TieredCompilation -XX:+UseCondCardMark
+ * volatile average:28ms [111, 18, 12, 13, 34, 13, 13, 13, 30, 28]
+ * 不多说, 使用/不使用 UseCondCardMark 差距明显
+ * jdk11.0.7 上运行
+ * 不使用
+ * volatile average:32ms [103, 18, 32, 17, 12, 35, 34, 32, 31, 15]
+ * 使用 -XX:+UseCondCardMark
+ * volatile average:28ms [73, 17, 13, 33, 33, 22, 13, 13, 33, 32]
+ * volatile average:30ms [63, 35, 31, 13, 19, 36, 32, 13, 32, 33]
+ * 使用 -XX:-TieredCompilation
+ * volatile average:35ms [106, 22, 13, 34, 32, 34, 34, 34, 13, 33]
+ * volatile average:29ms [108, 26, 33, 31, 31, 13, 13, 12, 13, 13]
+ * 使用 -XX:-TieredCompilation -XX:+UseCondCardMark
+ * volatile average:36ms [99, 22, 13, 34, 35, 33, 33, 32, 34, 32]
+ * volatile average:20ms [71, 23, 13, 14, 14, 14, 14, 14, 13, 13]
+ * jdk11 中可能有了其他的实现和优化手段, 两种差距并不大
  *
  * @author GiraffeTree
  * @date 2020/5/18
@@ -40,9 +49,6 @@ public class CardTableFalseSharingTest {
 
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 1; i++) {
-            Thread.sleep(1000L);
-        }
 
         int loop = 10_000_000;
         int retry = 10;
@@ -56,42 +62,41 @@ public class CardTableFalseSharingTest {
         // 在同一个线程中分配两个对象, 理论上这两个对象相距很近
 
         Obj64 x1 = new Obj64();
-        // 策略1: 防止 x1, x2 在同一缓存行
-        Obj64 padding = new Obj64();
-
-        // 策略2: 中间间隔大于 32KB (一个缓存行大小64B的卡表的 , 对应的实际内存为 32KB)
-//        byte[] padding = new byte[32 * 1024];
 
         Obj64 x2 = new Obj64();
-        testX1AndX2(loop, retry, x1, x2);
+        // 大对象直接进入老年代, 为了得到一个 老年代 -> eden 代的引用
+        HugeObj hugeObj10M = new HugeObj(10 * 1024 * 1024, x1, x2);
+        testX1AndX2(loop, retry, x1, x2, hugeObj10M);
     }
 
-    private static void testX1AndX2(final int loop, final int retry, final Obj64 x1, final Obj64 x2) {
+    private static void testX1AndX2(final int loop, final int retry, final Obj64 x1, final Obj64 x2, final HugeObj hugeObj) {
         long all = 0L;
         int r = retry;
         long[] record = new long[retry];
         while (r-- > 0) {
             final CountDownLatch endLatch = new CountDownLatch(2);
             long l1 = System.currentTimeMillis();
-            EXECUTOR_SERVICE.execute(new Runnable() {
-                @Override
-                public void run() {
-                    int count = loop;
-                    while (count-- > 0) {
-                        x1.l1 = count;
+            EXECUTOR_SERVICE.execute(() -> {
+                int count = loop;
+                while (count-- > 0) {
+                    if ((count & 1) == 0) {
+                        hugeObj.x1 = x1;
+                    } else {
+                        hugeObj.x1 = x2;
                     }
-                    endLatch.countDown();
                 }
+                endLatch.countDown();
             });
-            EXECUTOR_SERVICE.execute(new Runnable() {
-                @Override
-                public void run() {
-                    int count = loop;
-                    while (count-- > 0) {
-                        x2.l1 = count;
+            EXECUTOR_SERVICE.execute(() -> {
+                int count = loop;
+                while (count-- > 0) {
+                    if ((count & 1) == 0) {
+                        hugeObj.x2 = x2;
+                    } else {
+                        hugeObj.x2 = x1;
                     }
-                    endLatch.countDown();
                 }
+                endLatch.countDown();
             });
 
             try {
@@ -108,8 +113,20 @@ public class CardTableFalseSharingTest {
     }
 
     private static class Obj64 {
-        volatile long l1;
+        long l1;
         long l2, l3, l4, l5;
+    }
+
+    private static class HugeObj {
+        private byte[] hugeArray;
+        private Obj64 x1;
+        private Obj64 x2;
+
+        public HugeObj(int byteLen, Obj64 x1, Obj64 x2) {
+            this.hugeArray = new byte[byteLen];
+            this.x1 = x1;
+            this.x2 = x2;
+        }
     }
 
 }
--- src/main/java/me/giraffetree/java/boomjava/concurrent/problem/falseShare/cardtable/README.md
+++ src/main/java/me/giraffetree/java/boomjava/concurrent/problem/falseShare/cardtable/README.md
@@ -2,7 +2,7 @@
 
 为了避免 minor GC (新生代GC) 对全堆的扫描, hotspot 引入了 cardTable 技术 [1]
 
-## 如何实现 卡表
+## 如何实现 卡表 [1]
 
 > 该技术将整个堆划分为一个个大小为 512 字节的卡，并且维护一个卡表，用来存储每张卡的一个标识位。
 > 这个标识位代表对应的卡是否可能存有指向新生代对象的引用。如果可能存在，那么我们就认为这张卡是脏的。
@@ -10,13 +10,13 @@
 > 在进行 Minor GC 的时候，我们便可以不用扫描整个老年代，而是在卡表中寻找脏卡，
 > 并将脏卡中的对象加入到 Minor GC 的 GC Roots 里。当完成所有脏卡的扫描之后，Java 虚拟机便会将所有脏卡的标识位清零。
 
-### 实现
+### 实现 [1]
 
 首先，如果想要保证每个可能有指向新生代对象引用的卡都被标记为脏卡，
 那么 Java 虚拟机需要截获每个引用型实例变量的写操作，并作出对应的写标识位操作。
 
 这个操作在解释执行器中比较容易实现。但是在即时编译器生成的机器码中，
-则需要插入额外的逻辑。这也就是所谓的写屏障（write barrier，注意不要和 volatile 字段的写屏障混淆）。
+则需要插入额外的逻辑。这也就是所谓的写屏障（write barrier）。
 
 ```
 CARD_TABLE [this address >> 9] = DIRTY;
@@ -27,9 +27,22 @@ CARD_TABLE [this address >> 9] = DIRTY;
 
 ##  虚共享
 
-- TLAB 分配给线程的大小为 512字节
 - 卡表false sharing 对应的实际内存大小为 32KB
 
+### UseCondCardMark
+
+`-XX:+UseCondCardMark` 通过以下操作来减少 card table 的写入, 从而提高性能 [1]
+
+```
+if (CARD_TABLE [this address >> 9] != DIRTY) 
+  CARD_TABLE [this address >> 9] = DIRTY;
+```
+
+### 测试
+
+重要的是, 要构造一个 老年代 -> eden 代的引用 [4]
+
+[CardTableFalseSharingTest](./CardTableFalseSharingTest.java)
 
 
 ## 参考
@@ -40,3 +53,6 @@ CARD_TABLE [this address >> 9] = DIRTY;
     - http://robsjava.blogspot.com/2013/03/why-to-use-conditional-card-marking.html
 3. `-XX：+ UseCondCardMark` 可以在高度并行的应用程序中帮助您
     - https://stackoverflow.com/questions/28568171/examples-for-code-that-is-surprisingly-affected-by-jvm-jvm-options
+4. PretenureSizeThreshold 的默认值和作用
+    - https://juejin.im/post/5d1b048ff265da1bb67a326e
+
